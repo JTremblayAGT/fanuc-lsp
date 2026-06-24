452fce4670ffbef924aa9c213dd712b796ef7469
Justin Tremblay <justin.tremblay@agt-group.com>
2026-06-24 09:43:33 -0400

    add Karel external references provider, shows karel var usages in TPP programs from a kl program

--- FanucLSP/Lsp/References/KlExternalReferenceProvider.cs
+++ FanucLSP/Lsp/References/KlExternalReferenceProvider.cs
@@ -15,6 +15,30 @@ internal sealed class KlExternalReferenceProvider : IKlReferenceProvider
         LspServerState state
     )
     {
-        throw new NotImplementedException();
+        var token = ProgramUtils.GetTokenAt(document.Text, position);
+        if (string.IsNullOrWhiteSpace(token))
+        {
+            return [];
+        }
+
+        // Don't check in routine scopes, only top-level variables can be referenced outside the program
+        if (program.SymTable.GetSymbol(token) is not { } symbol)
+        {
+            return [];
+        }
+
+        var fullName = $"$[{program.Name}]{symbol.FullName}";
+        return state.AllTextDocuments.Values
+            .Where(doc => doc.Program is TppProgram)
+            .SelectMany(doc => ((TppProgram)doc.Program!).Program.SymTable.GetSymbolReferences(fullName)
+                    .Select(refpos => new TextDocumentLocation { Uri = doc.TextDocument.Uri, Range = GetContentRange(refpos) }))
+            .ToArray();
     }
+
+    private ContentRange GetContentRange(TokenPosition position)
+        => new()
+        {
+            Start = new ContentPosition { Line = position.Line - 1, Character = position.Column - 1 },
+            End = new ContentPosition { Line = position.Line - 1, Character = position.Column - 1 }
+        };
 }
